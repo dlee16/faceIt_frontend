3c3738dcd9e9cc8bb18575b7d376e8eda2e78aac
dlee16 <diana.lee116@gmail.com>
2019-08-30 15:34:33 -0400

    added additional login functions

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,12 +6,44 @@ class Login extends React.Component{
         password: "",
         error: ""
     }
+    
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    errorDisplay = (error) => {
+        this.setState({
+            error
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+
+        fetch('http://localhost:3000/login', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(this.state),
+        })
+            .then(res => res.json())
+            .then(response => {
+                if (response.errors) {
+                    this.errorDisplay(response.errors)
+                } else {
+                    this.props.setCurrentUser(response)
+                }
+            })
+    }
 
     render(){
         return(
             <div>
                 Login to your account
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <input type="text" name="username" placeholder="username"/>
                     <input type="password" name="password" placeholder="password"/>
                     <button>Login</button>
