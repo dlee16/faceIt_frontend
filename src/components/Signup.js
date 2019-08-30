42c9b5bda4dc5bd7444d3d3ad2622564ce5e9a42
dlee16 <diana.lee116@gmail.com>
2019-08-30 15:06:31 -0400

    signup page form and links setup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,10 +1,45 @@
 import React from 'react';
 
+const initialState = {
+    name:"",
+    username:"",
+    password:""
+}
+
 class Signup extends React.Component{
+    state = initialState
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.props.createUser(this.state)
+        this.setState(initialState)
+    }
+
+    handleClick = () => {
+        this.props.history.push('/login')
+    }
+
     render(){
         return(
         <div>
-            THIS IS THE SIGNUP PAGE
+            <form onSubmit={this.handleSubmit}>
+                <label>Name:
+                    <input onChange={this.handleChange} value={this.state.name} type="text" name="name" placeholder="Enter your name" />
+                </label>
+                <label>Username:
+                    <input onChange={this.handleChange} value={this.state.username} type="text" name="username" placeholder="Enter your username" />
+                </label>
+                <label>Password:
+                    <input onChange={this.handleChange} value={this.state.password} type="password" name="password" placeholder="Enter your password" />
+                </label>
+                <button onClick={this.handleClick}>Sign Up! </button>
+            </form>
         </div>
         )
     }
