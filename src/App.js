8b81e4242307ff9b2a8d0a307a7feeec30fe615e
dlee16 <diana.lee116@gmail.com>
2019-08-30 14:18:52 -0400

    buttons and route for sign in and sign up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import './App.css';
-import {Route ,Switch} from 'react-router-dom';
+import {Route ,Switch, withRouter} from 'react-router-dom';
 import Login from './components/Login';
 import Nav from './components/Nav';
+import Signup from './components/Signup';
 import ImageCard from './components/ImageCard';
 import Welcome from './components/Welcome';
 import Error from './components/Error';
+import {connect} from 'react-redux';
 
 
 class App extends React.Component{
   render(){
     return(
       <div className="App">
-        This is the main page
         <Nav />
         <Switch>
           <Route path='/images' component={ImageCard} />
           <Route path='/login' component={Login} />
+          <Route path='/signup' component={Signup} />
           <Route exact path='/' component={Welcome} />
           <Route component={Error} />
         </Switch>
@@ -25,4 +27,12 @@ class App extends React.Component{
   }
 }
 
-export default App;
+const mapStateToProps = (state)=> {
+  return{
+
+  }
+}
+
+
+
+export default connect(mapStateToProps)(withRouter(App));
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,6 @@ class Nav extends React.Component{
     render(){
         return(
             <div>
-                This is my nav bar
                 <NavLink to='/'>Log Out </NavLink>
                 <NavLink to='/login'>Log In </NavLink>
             </div>
