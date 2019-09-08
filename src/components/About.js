cadce6de1c097f6b1ab8e4366df34026a1ea994e
dlee16 <diana.lee116@gmail.com>
2019-09-08 10:37:50 -0400

    about page

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class About extends React.Components{
+    render(){
+        return(
+            <div>
+                This is the about page. 
+            </div>
+        )
+    }
+}
+
+export default About;
